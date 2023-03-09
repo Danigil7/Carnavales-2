7800908929786876bcefcddf0405b17244da4dab
Danigil7 <117642693+Danigil7@users.noreply.github.com>
2023-03-09 17:06:34 +0100

    Documentacion clase Romancero
    Documentacion clase Romancero del paquete model

--- src/model/Romancero.java
+++ src/model/Romancero.java
@@ -1,4 +1,14 @@
 
+/**
+ * 
+ * @author danigil
+ * @version v1.2023
+ * @since 3-2023
+ * 
+ * <p>Esta clase Romancero, se encarga de la especialidad de agrupaciones, Romancero</p>
+ *
+ */
+
 package model;
 
 public class Romancero extends Agrupaciones implements Callejera{
@@ -6,17 +16,32 @@ public class Romancero extends Agrupaciones implements Callejera{
 	private String tematicaCartel;
 	
 
+	/**
+	 * Metodo constructor sin parametros
+	 */
 	public Romancero() {
 		super();
 		setTematicaCartel("");
 	}
 	
-	
+	/**
+	 * Metodo constructor con parametros
+	 * @param nombre
+	 * @param autor
+	 * @param autorMusica
+	 * @param autorLetra
+	 * @param tipoDisfraz
+	 * @param tematicaCartel
+	 */
 	public Romancero(String nombre, String autor, String autorMusica, String autorLetra, String tipoDisfraz, String tematicaCartel) {
 		super(nombre, autor, autorMusica, autorLetra, tipoDisfraz);
 		setTematicaCartel(tematicaCartel);
 	}
 	
+	/**
+	 * Getters and Setters
+	 * @return tematicaCartel
+	 */
 
 	public String getTematicaCartel() {
 		return tematicaCartel;
@@ -27,6 +52,9 @@ public class Romancero extends Agrupaciones implements Callejera{
 	}
 
 
+	/**
+	 * Este metodo sobreescribe el toString() de la clase padre. Devuelve una cadena de caracteres 
+	 */
 	@Override
 	public String toString() {
 		return "Romancero [getTematicaCartel()=" + getTematicaCartel() + ", getNombre()=" + getNombre()
@@ -34,20 +62,26 @@ public class Romancero extends Agrupaciones implements Callejera{
 				+ getAutorLetra() + ", getTipoDisfraz()=" + getTipoDisfraz() + "]";
 	}
 
-
+	/**
+	 * Este metodo sobreescribe el toString() de la clase padre. Devuelve una cadena de caracteres 
+	 */
 	@Override
 	public String cantarPresentacion() {
 		String cadena;
 		return cadena = "Cantando la presentación del Romancero " + getNombre();
 	}
-
+	/**
+	 * Este metodo sobreescribe el toString() de la clase padre. Devuelve una cadena de caracteres 
+	 */
 	@Override
 	public String hacerTipo() {
 		String cadena;
 		return cadena = "El Romancero " + getNombre() + " va de " + getTipoDisfraz();
 
 	}
-
+	/**
+	 * Este metodo sobreescribe el toString() de la clase padre. Devuelve una cadena de caracteres 
+	 */
 
 	@Override
 	public String amoAEscucha() {
@@ -55,7 +89,9 @@ public class Romancero extends Agrupaciones implements Callejera{
 		return cadena = "Amo a escuchá el Romancero " + super.getNombre();
 	}
 
-
+	/**
+	 * Este metodo sobreescribe el toString() de la clase padre. Devuelve una cadena de caracteres 
+	 */
 	@Override
 	public int compareTo(Agrupaciones o) {
 		int comp;
@@ -64,12 +100,5 @@ public class Romancero extends Agrupaciones implements Callejera{
 		
 	}
 
-
-	
-	
-	
-	
-	
-	
 	
 }
