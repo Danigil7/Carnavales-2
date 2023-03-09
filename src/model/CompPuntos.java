6af06fd8cd5cbce55f2e480f29c59c837d4377f2
Danigil7 <117642693+Danigil7@users.noreply.github.com>
2023-03-09 16:56:43 +0100

    Documentacion de la clase CompPuntos
    Documentacion de la clase CompPuntos del paquete model

--- src/model/CompPuntos.java
+++ src/model/CompPuntos.java
@@ -1,9 +1,23 @@
+
+/**
+ * 
+ * @author danigil
+ * @version v1.2023
+ * @since 3-2023
+ * 
+ * <p>Esta clase CompPuntos, se encarga de los puntos de cada especialidad de agrupaciones</p>
+ *
+ */
+
 package model;
 
 import java.util.Comparator;
 
 public class CompPuntos implements Comparator <AgrupacionOficial>{
 
+	/**
+	 * Este metodo sobreescribe el toString() de la clase padre. Devuelve una cadena de caracteres 
+	 */
 	@Override
 	public int compare(AgrupacionOficial o1, AgrupacionOficial o2) {
 		int comp;
