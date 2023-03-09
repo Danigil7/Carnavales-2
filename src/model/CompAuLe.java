dd576233b391757097a93eeb68dd2f70a9a86edb
Danigil7 <117642693+Danigil7@users.noreply.github.com>
2023-03-09 16:50:42 +0100

    Documentacion clase CompAuLe
    Documentacion clase CompAuLe del paquete model

--- src/model/CompAuLe.java
+++ src/model/CompAuLe.java
@@ -1,9 +1,23 @@
+
+/**
+ * 
+ * @author danigil
+ * @version v1.2023
+ * @since 3-2023
+ * 
+ * <p>Esta clase Chirigota, se encarga de los compositores y autores de la letra de las agrupaciones</p>
+ *
+ */
+
 package model;
 
 import java.util.Comparator;
 
 public class CompAuLe implements Comparator <Agrupaciones>{
 
+	/**
+	 * Este metodo sobreescribe el toString() de la clase padre. Devuelve una cadena de caracteres 
+	 */
 	@Override
 	public int compare(Agrupaciones o1, Agrupaciones o2) {
 		int comp = 0;
