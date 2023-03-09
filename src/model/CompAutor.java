b017449b93108d6406de025e73b19b9a5ae4f68c
Danigil7 <117642693+Danigil7@users.noreply.github.com>
2023-03-09 16:52:41 +0100

    Documentacion de la clase CompAutor
    Documentacion de la clase CompAutor del paquete model

--- src/model/CompAutor.java
+++ src/model/CompAutor.java
@@ -1,9 +1,23 @@
+
+/**
+ * 
+ * @author danigil
+ * @version v1.2023
+ * @since 3-2023
+ * 
+ * <p>Esta clase Chirigota, se encarga de los compositores y autores las agrupaciones</p>
+ *
+ */
+
 package model;
 
 import java.util.Comparator;
 
 public class CompAutor implements Comparator<Agrupaciones> {
 
+	/**
+	 * Este metodo sobreescribe el toString() de la clase padre. Devuelve una cadena de caracteres 
+	 */
 	@Override
 	public int compare(Agrupaciones o1, Agrupaciones o2) {
 
