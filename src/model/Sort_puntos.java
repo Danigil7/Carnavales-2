07a3c80d9a35aa8353948f7329f667653a54f186
Danigil7 <117642693+Danigil7@users.noreply.github.com>
2023-03-09 17:08:03 +0100

    Documentacion clase Sort_puntos
    Documentacion clase Sort_puntos paquete model

--- src/model/Sort_puntos.java
+++ src/model/Sort_puntos.java
@@ -1,9 +1,23 @@
+
+/**
+ * 
+ * @author danigil
+ * @version v1.2023
+ * @since 3-2023
+ * 
+ * <p>Esta clase Sort_puntos, se encarga del sorteo de los puntos</p>
+ *
+ */
+
 package model;
 
 import java.util.Comparator;
 
 public class Sort_puntos implements Comparator <AgrupacionOficial>{
 
+	/**
+	 * Este metodo sobreescribe el toString() de la clase padre. Devuelve una cadena de caracteres 
+	 */
 	@Override
 	public int compare(AgrupacionOficial o1, AgrupacionOficial o2) {
 		
