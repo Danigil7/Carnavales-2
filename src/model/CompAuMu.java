febd3ec40992518db12eca8f077fd7b22d718497
Danigil7 <117642693+Danigil7@users.noreply.github.com>
2023-03-09 16:54:24 +0100

    Documentacion de la clase CompAuMu
    Documentacion de la clase CompAuMu del paquete model

--- src/model/CompAuMu.java
+++ src/model/CompAuMu.java
@@ -5,7 +5,7 @@
  * @version v1.2023
  * @since 3-2023
  * 
- * <p>Esta clase Chirigota, se encarga de los compositores y autores de la musica de las agrupaciones</p>
+ * <p>Esta clase CompAuMu, se encarga de los compositores y autores de la musica de las agrupaciones</p>
  *
  */
 
