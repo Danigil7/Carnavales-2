e71012bd4b99ff7baf461767e8e3c4ce4c453c9c
Danigil7 <117642693+Danigil7@users.noreply.github.com>
2023-03-09 16:41:14 +0100

    Documentacion clase AgrupacionOficial
    Documentacion de la clase AgrupacionOficial del paquete model

--- src/model/AgrupacionOficial.java
+++ src/model/AgrupacionOficial.java
@@ -1,4 +1,14 @@
 
+/**
+ * 
+ * @author danigil
+ * @version v1.2023
+ * @since 3-3-2023
+ * 
+ * <p>Esta clase Agrupaciones, </p>
+ *
+ */
+
 package model;
 
 import java.util.Arrays;
@@ -9,18 +19,34 @@ public abstract class AgrupacionOficial extends Agrupaciones{
 	private Integrante[] integrantes;
 	private Integer puntos;
 
+	/**
+	 * Constructor de la clase
+	 */
 	public AgrupacionOficial() {
 		super();
 		setIntegrantes(new Integrante[10]);
 
 	}
 
+	/**
+	 * este metodo es otro constructor de la clase pero este tiene parametros
+	 * @param nombre
+	 * @param autor
+	 * @param autorMusica
+	 * @param autorLetra
+	 * @param tipoDisfraz
+	 * @param numIntegrante
+	 */
 	public AgrupacionOficial(String nombre, String autor, String autorMusica, String autorLetra, String tipoDisfraz,
 			Integrante[] numIntegrante) {
 		super(nombre, autor, autorMusica, autorLetra, tipoDisfraz);
 		setIntegrantes(numIntegrante);
 	}
 
+	/**
+	 * 
+	 * Getters and Setters
+	 */
 	public Integrante[] getIntegrantes() {
 		return integrantes;
 	}
@@ -39,6 +65,11 @@ public abstract class AgrupacionOficial extends Agrupaciones{
 		this.puntos = puntos;
 	}
 
+	/**
+	 * busca la primera posición que este vacia o con valor null. Si la encuentra inserta el integrante y pone la variable insertar en true, si no se queda en false
+	 * @param integ
+	 * @return boolean
+	 */
 	public boolean insertarIntegrante(Integrante integ) {
 		boolean insertar = false;
 		for (int i = 0; i < integrantes.length; i++) {
@@ -53,7 +84,14 @@ public abstract class AgrupacionOficial extends Agrupaciones{
 
 		return insertar;
 	}
+	
 
+	/**
+	 * este método elimina un integrante del arreglo de integrantes de la instancia actual de la clase AgrupacionOficial. Si encuentra la posición del integrante, establece el valor 
+	 * de esa posición como null y devuelve true. Si no encuentra la posición del integrante, devuelve false.
+	 * @param integ
+	 * @return boolean
+	 */
 	public boolean eliminarIntegrante(Integrante integ) {
 		boolean eliminar = false;
 		for (int i = 0; i < integrantes.length; i++) {
@@ -71,7 +109,9 @@ public abstract class AgrupacionOficial extends Agrupaciones{
 	public abstract String caminitoDelFalla();
 	
 	
-	
+	/**
+	 * Este metodo sobreescribe el toString() de la clase padre. Devuelve una cadena de caracteres 
+	 */
 	@Override
 	public String toString() {
 		return "AgrupacionOficial \nNumIntegrante()=" + Arrays.toString(getIntegrantes()) + ", \nPuntos()="
