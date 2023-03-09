36e150c0cfa8fc32e923b0b393be14435022c41d
Danigil7 <117642693+Danigil7@users.noreply.github.com>
2023-03-09 16:42:49 +0100

    Documentacion clase Callejera
    Documentacion clase Callejera del paquete model

--- src/model/Callejera.java
+++ src/model/Callejera.java
@@ -1,4 +1,12 @@
-
+/**
+ * 
+ * @author danigil
+ * @version v1.2023
+ * @since 3-3-2023
+ * 
+ * <p>Esta clase Callejera, se encarga de la especialidad de agrupaciones, callejeras</p>
+ *
+ */
 package model;
 
 public interface Callejera {
